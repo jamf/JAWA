2aa936874b1a61db113d5cc7b8219bd05caff138
ball42 <christopher.ball@jamf.com>
2021-06-16 02:58:00 -0500

    /wizard now uses tags to organize Jamf Pro & Okta webhooks

--- webapp.py
+++ webapp.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 # encoding: utf-8
-import io
-import os
-import json
-import glob
-import logging
-from time import sleep
-import requests
+from collections import defaultdict
 from flask import (Flask, request, render_template,
                    session, redirect, url_for, escape)
+import glob
+import json
+import logging
+import os
+import requests
 from waitress import serve
 
 # Flask logging
@@ -17,14 +16,8 @@ logger.setLevel(logging.INFO)
 error_message = ""
 verify_ssl = True  # Enables Jamf Pro SSL certificate verification
 
-# global jamf_url
-# global jamf_username
-# global jamf_password
-# global distilled_serial
-app = Flask(__name__)
-
-
 # Initiate Flask
+app = Flask(__name__)
 
 
 def main():
@@ -32,19 +25,16 @@ def main():
     print(base_dir)
     environment_setup(base_dir)
     register_blueprints()
-    app.secret_key = "*"
+    app.secret_key = "untini"
     serve(app, url_scheme='https', host='0.0.0.0', port=8000)
 
 
 def environment_setup(project_dir):
-    global jp_file, okta_file, cron_file, server_json_file, scripts_directory
-    # webhook_file = os.path.join(project_dir, 'data', 'webhook.conf')
-    jp_file = os.path.join(project_dir, 'data', 'jp_webhooks.json')
-    okta_file = os.path.join(project_dir, 'data', 'okta_json.json')
+    global jp_file, cron_file, server_json_file, scripts_directory
+    jp_file = os.path.join(project_dir, 'data', 'webhooks.json')
     cron_file = os.path.join(project_dir, 'data', 'cron.json')
     server_json_file = os.path.join(project_dir, 'data', 'server.json')
     scripts_directory = os.path.join(project_dir, 'scripts')
-    # return webhook_file, jp_file, okta_file, cron_file, server_json_file, scripts_directory
 
 
 def register_blueprints():
@@ -80,26 +70,23 @@ def register_blueprints():
 @app.route('/setup', methods=['GET', 'POST'])
 def setup():
     if 'username' in session:
-
-        # global jawa_address
-
         if request.method == 'POST':
             server_url = request.form.get('address')
             jps_url = request.form.get('jss-lock')
             new_json = {}
             if server_url != '':
                 new_json['jawa_address'] = server_url
-            print(server_url)
+            # print(server_url)
             if jps_url != '':
                 new_json['jps_url'] = jps_url
-            print(new_json)
+            # print(new_json)
             if not os.path.isfile(server_json_file):
                 with open(server_json_file, "w") as outfile:
                     server_json = {'jawa_address': server_url, 'jps_url': jps_url}
                     json.dump(server_json, outfile)
             elif os.path.isfile(server_json_file):
                 with open(server_json_file, "w") as outfile:
-                    server_json = [{'jawa_address': server_url, 'jps_url': jps_url}]
+                    server_json = {'jawa_address': server_url, 'jps_url': jps_url}
                     json.dump(server_json, outfile)
                 with open(server_json_file, "r") as fin:
                     data = json.load(fin)
@@ -207,7 +194,7 @@ def index():
     else:
         with open(server_json_file) as json_file:
             server_json = json.load(json_file)
-        print(server_json)
+        # print(server_json)
 
         if not 'jps_url' in server_json:
             return render_template('home.html')
@@ -233,7 +220,7 @@ def home():
         else:
             with open(server_json_file) as json_file:
                 server_json = json.load(json_file)
-            print(server_json)
+            # print(server_json)
             if not 'jps_url' in server_json:
                 return render_template('home.html')
             elif server_json['jps_url'] is None:
@@ -254,51 +241,56 @@ def home():
 def wizard():
     with open(jp_file) as webhook_json:
         webhooks_installed = json.load(webhook_json)
-        webhook_json = webhooks_installed
+    jamf_pro_webhooks = []
+    okta_webhooks = []
+    for each_webhook in webhooks_installed:
+        data = defaultdict(lambda: "MISSING", each_webhook)
+        tag = data['tag']
+        if tag == "jamfpro":
+            jamf_pro_webhooks.append(each_webhook)
+        elif tag == "okta":
+            okta_webhooks.append(each_webhook)
 
-        response = requests.get(
-            session['url'] + '/JSSResource/webhooks',
-            auth=(session['username'], session['password']),
-            headers={'Accept': 'application/json'},
-            verify=verify_ssl)
+    # print(f"Full JP Webhook list: {jamf_pro_webhooks}")
 
-        found_jamf_webhooks = response.json()['webhooks']
+    response = requests.get(
+        session['url'] + '/JSSResource/webhooks',
+        auth=(session['username'], session['password']),
+        headers={'Accept': 'application/json'},
+        verify=verify_ssl)
 
-        x = 0
-        jamf_webhooks = []
-        while True:
-            try:
-                jamf_webhooks.append(found_jamf_webhooks[x]['name'])
-                x += 1
-                str_error = None
-            except Exception as str_error:
-                pass
-                if str_error:
-                    sleep(2)
-                    break
-                else:
-                    continue
+    found_jamf_webhooks = response.json()['webhooks']
 
-        for webhook in webhooks_installed:
-            if webhook['name'] in jamf_webhooks:
-                webhook_endpoint = '/JSSResource/webhooks/name/'
-                response = requests.get(
-                    session['url'] + webhook_endpoint + webhook['name'],
-                    auth=(session['username'], session['password']),
-                    headers={'Accept': 'application/json'},
-                    verify=verify_ssl)
+    jamf_webhooks = []
 
-                response_json = response.json()
+    x = 0
+    while True:
+        try:
+            jamf_webhooks.append(found_jamf_webhooks[x]['name'])
+            x += 1
+            str_error = None
+        except Exception as str_error:
+            pass
+            if str_error:
+                # sleep(2)
+                break
+            else:
+                continue
 
-                jamf_event = response_json['webhook']['event']
-                jamf_id = response_json['webhook']['id']
-                print(webhook['script'])
-                script = webhook['script']
-                # webhook_json.append({"name": webhook['name'],
-                #                      "jamf_id": jamf_id,
-                #                      "event": jamf_event,
-                #                      "script": script,
-                #                      "description": webhook['description']})
+    for webhook in webhooks_installed:
+        if webhook['name'] in jamf_webhooks:
+            webhook_endpoint = '/JSSResource/webhooks/name/'
+            response = requests.get(
+                session['url'] + webhook_endpoint + webhook['name'],
+                auth=(session['username'], session['password']),
+                headers={'Accept': 'application/json'},
+                verify=verify_ssl)
+
+            response_json = response.json()
+
+            jamf_event = response_json['webhook']['event']
+            jamf_id = response_json['webhook']['id']
+            script = webhook['script']
 
     data = []
 
@@ -316,39 +308,23 @@ def wizard():
                                "script": script[1],
                                "description": cron['description']})
 
-    data = []
-
-    if not os.path.isfile(okta_file):
-        with open(okta_file, "w") as outfile:
-            json.dump(data, outfile)
-
-    with open(okta_file) as okta_json:
-        oktas_installed = json.load(okta_json)
-        oktas_json = []
-        for okta in oktas_installed:
-            script = okta['script'].rsplit('/', 1)
-            oktas_json.append({"name": okta['name'],
-                               "event": okta['okta_event'],
-                               "script": script[1]})
-
     webhook_url = session['url']
 
     if not webhook_json:
         webhook_json = ''
     if not crons_json:
         crons_json = ''
-    if not oktas_json:
-        oktas_json = ''
+    # if not oktas_json:
+    #     oktas_json = ''
 
-    if webhook_json == crons_json == oktas_json:
+    if webhook_json == crons_json:
         return redirect(url_for('first_automation'))
-
     return render_template(
         'wizard.html',
         webhook_url=webhook_url,
-        webhooks_installed=webhook_json,
+        webhooks_installed=jamf_pro_webhooks,
         crons_installed=crons_json,
-        oktas_installed=oktas_json,
+        oktas_installed=okta_webhooks,
         login="true",
         username=str(escape(session['username'])))
 

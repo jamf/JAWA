6730e5eb80cc2068ba4bafd57c6d6615916606d2
ball42 <christopher.ball@jamf.com>
2021-07-01 01:32:41 -0500

    The layout abides this jawa

--- webhook/jawa_receiver.py
+++ webhook/jawa_receiver.py
@@ -1,9 +1,11 @@
+
 from bin import okta_verification
 import flask
-from flask import session, request, redirect, url_for, render_template
+from flask import session, request, redirect, url_for, render_template, current_app
 # from glob import escape
 import json
-import logging
+
+from main import jawa_logger
 import os
 # import requests
 import subprocess
@@ -12,8 +14,9 @@ server_json_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 jp_webhooks_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'webhooks.json'))
 scripts_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'scripts'))
 
+
 blueprint = flask.Blueprint('jawa_receiver', __name__, template_folder='templates')
-logger = logging.getLogger('waitress')
+
 
 
 def validate_webhook(webhook_data, webhook_name, webhook_user, webhook_pass):
@@ -36,15 +39,17 @@ def run_script(webhook_data, webhook_name):
         webhooks_json = json.load(fin)
     for each_webhook in webhooks_json:
         if each_webhook['name'] == webhook_name:
-            subprocess.Popen([each_webhook['script'], f"{webhook_data}"])
+            proc = subprocess.Popen([each_webhook['script'], f"{webhook_data}"], stdout=subprocess.PIPE)
+            output = proc.stdout.read()
+            jawa_logger().info(output)
 
 
 @blueprint.route('/hooks/<webhook_name>', methods=['POST', 'GET'])
 def webhook_handler(webhook_name):
-    print(webhook_name)
+    jawa_logger().info(webhook_name)
     webhook_data = request.get_json()
     if request.headers.get('x-okta-verification-challenge'):
-        print("This is an Okta verification challenge...")
+        jawa_logger().info("This is an Okta verification challenge...")
         return okta_verification.verify_new_webhook(request.headers.get('x-okta-verification-challenge'))
 
     auth = request.authorization
@@ -61,9 +66,9 @@ def webhook_handler(webhook_name):
     #       f"Webhook data: {webhook_data}")
 
     if validate_webhook(webhook_data, webhook_name, webhook_user, webhook_pass):
-        print(f"{webhook_name} validated!")
+        jawa_logger().info(f"{webhook_name} validated!")
         run_script(webhook_data, webhook_name)
     else:
-        print(f"{webhook_name} not validated!")
+        jawa_logger().info(f"{webhook_name} not validated!")
 
     return webhook_data

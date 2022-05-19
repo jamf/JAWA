f28a20c5335a0ecb96b13e833385102736aa74a1
ball42 <christopher.ball@jamf.com>
2022-05-18 22:31:55 -0500

    optimized imports and the verify_new_webhook() method

--- bin/okta_verification.py
+++ bin/okta_verification.py
@@ -1,16 +1,5 @@
-#!/usr/bin/python
-
-from flask import request
-import json
-import sys
-
-
 def verify_new_webhook(challenge):
-    # challenge = sys.argv[2]
-    # verification = request.headers.get('x-okta-verification-challenge')
-    json_text = {f"verification": f"{challenge}"}
-    print(json.dumps(json_text))
-    return json_text
+    return {"verification": f"{challenge}"}
 
 
 def main():

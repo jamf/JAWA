c5717720b806a252491f366b1b0870f079dff63c
ball42 <christopher.ball@jamf.com>
2022-05-18 22:30:18 -0500

    added log_level parameter and parameter defaults to setup_logger(), added parameters to setup_child_logger()

--- bin/logger.py
+++ bin/logger.py
@@ -4,38 +4,28 @@ import os
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger_name = 'jawa'
+default_log_level = logging.INFO
 log_roll_size = (1048576 * 100)
 log_backupCount = 10
 
 
-def setup_logger(log_name, log_filename):
-    # log_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'jawa.log'))
-    logging.basicConfig(level=logging.INFO)
+def setup_logger(log_name=logger_name, log_filename=f"{logger_name}.log", log_level=default_log_level):
     log_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
-    print(f'log_path: {log_path}')
     if not os.path.isdir(log_path):
         os.makedirs(log_path)
     log_file = os.path.join(log_path, log_filename)
-    print(f'log_file: {log_file}')
     handler = handlers.RotatingFileHandler(log_file, maxBytes=log_roll_size, backupCount=log_backupCount)
     handler.setFormatter(formatter)
     logger = logging.getLogger(log_name)
     if logger.hasHandlers():
         logger.handlers.clear()
     logger.addHandler(handler)
-    # if settings.debug_mode:
-    #     logger.setLevel(logging.DEBUG)
-    # else:
-    logger.setLevel(logging.DEBUG)
-    print('created logger.')
+    logger.setLevel(log_level)
     return logger
 
 
-def setup_child_logger(name):
-    return logging.getLogger(logger_name).getChild(name)
+def setup_child_logger(name_of_logger, name_of_child):
+    return logging.getLogger(name_of_logger).getChild(name_of_child)
 
 
-# print('going to create logthis.')
 logthis = setup_logger(logger_name, f'{logger_name}.log')
-# print('created logthis.')
-# logthis.debug('this got logged by the main logger')

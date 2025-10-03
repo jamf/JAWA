a983b6e14f8b38e892f825a607d36bd64ac01edf
Chris Ball <47579551+ball42@users.noreply.github.com>
2025-10-03 15:42:08 -0500

    fix: resolve #53, Style & Standards (#56)
    * chore: add type hints, add helper method for loading JSON
    
    * chore: add type hints
    
    * chore: add ruff config for code formatting
    
    * chore: add type hints, format code to PEP standards with ruff
    
    * chore: ignore ruff cache
    
    * chore: default to double quotes instead of single quotes
    
    * refactor: format codebase with double quotes for consistency
    
    * fix: corrected notice info for custom webhooks with custom auth
    
    ---------
    
    Co-authored-by: Andrew Lerman <aspeciale88@gmail.com>

--- bin/logger.py
+++ bin/logger.py
@@ -29,20 +29,31 @@
 import logging
 from logging import handlers
 import os
+from typing import Optional
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger_name = 'jawa'
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger_name = "jawa"
 default_log_level = logging.INFO
-log_roll_size = (1048576 * 100)
+log_roll_size = 1048576 * 100
 log_backupCount = 10
 
 
-def setup_logger(log_name=logger_name, log_filename=f"{logger_name}.log", log_level=default_log_level):
-    log_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+def setup_logger(
+    log_name: Optional[str] = logger_name,
+    log_filename: Optional[str] = f"{logger_name}.log",
+    log_level: Optional[int] = default_log_level,
+) -> logging.Logger:
+    log_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "data")
+    )
     if not os.path.isdir(log_path):
         os.makedirs(log_path)
     log_file = os.path.join(log_path, log_filename)
-    handler = handlers.RotatingFileHandler(log_file, maxBytes=log_roll_size, backupCount=log_backupCount)
+    handler = handlers.RotatingFileHandler(
+        log_file, maxBytes=log_roll_size, backupCount=log_backupCount
+    )
     handler.setFormatter(formatter)
     logger = logging.getLogger(log_name)
     if logger.hasHandlers():
@@ -52,8 +63,10 @@ def setup_logger(log_name=logger_name, log_filename=f"{logger_name}.log", log_le
     return logger
 
 
-def setup_child_logger(name_of_logger, name_of_child):
+def setup_child_logger(
+    name_of_logger: str, name_of_child: str
+) -> logging.Logger:
     return logging.getLogger(name_of_logger).getChild(name_of_child)
 
 
-logthis = setup_logger(logger_name, f'{logger_name}.log')
+logthis = setup_logger(logger_name, f"{logger_name}.log")
--- views/log_view.py
+++ views/log_view.py
@@ -31,85 +31,149 @@ import re
 import subprocess
 from datetime import datetime
 from time import sleep
+from typing import Union
 
-from flask import (Blueprint, current_app, redirect, render_template, Response,
-                   request, send_file, session, url_for)
+from flask import (
+    Blueprint,
+    current_app,
+    redirect,
+    render_template,
+    Response,
+    request,
+    send_file,
+    session,
+    url_for,
+)
 
 from bin import logger
 from bin.view_modifiers import response
 
-logthis = logger.setup_child_logger('jawa', __name__)
+logthis = logger.setup_child_logger("jawa", __name__)
 
-log_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'jawa.log'))
-server_json_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'server.json'))
+log_file = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "data", "jawa.log")
+)
+server_json_file = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "data", "server.json")
+)
 
-blueprint = Blueprint('log_view', __name__, template_folder='templates')
+blueprint = Blueprint("log_view", __name__, template_folder="templates")
 
 
-@blueprint.route('/log/home.html', methods=['GET'])
-def log_home():
-    if 'username' not in session:
-        return redirect(url_for('home_view.logout', error_title="Session Timed Out", error_message="Please sign in again"))
-    logthis.debug(f"[{session.get('url')}] {session.get('username').title()} viewed {request.path}")
-    with open(log_file, 'r') as fin:
-        lines = [re.sub('\n', '', line) for line in fin.readlines()]
+@blueprint.route("/log/home.html", methods=["GET"])
+def log_home() -> Union[Response, str]:
+    if "username" not in session:
+        return redirect(
+            url_for(
+                "home_view.logout",
+                error_title="Session Timed Out",
+                error_message="Please sign in again",
+            )
+        )
+    logthis.debug(
+        f"[{session.get('url')}] {session.get('username').title()} viewed {request.path}"
+    )
+    with open(log_file, "r") as fin:
+        lines = [re.sub("\n", "", line) for line in fin.readlines()]
         lines.reverse()
         view_lines = lines[:500]
-    return render_template('log/home.html', username=session.get('username'), log=view_lines)
+    return render_template(
+        "log/home.html", username=session.get("username"), log=view_lines
+    )
 
 
-@blueprint.route('/log/view', methods=['GET'])
-def log_view():
-    if 'username' not in session:
-        return redirect(url_for('home_view.logout', error_title="Session Timed Out", error_message="Please sign in again"))
-    logthis.debug(f"[{session.get('url')}] {session.get('username').title()} viewed {request.path}")
-    with open(log_file, 'r') as fin:
-        lines = [re.sub('\n', '', line) for line in fin.readlines()]
+@blueprint.route("/log/view", methods=["GET"])
+def log_view() -> Union[Response, str]:
+    if "username" not in session:
+        return redirect(
+            url_for(
+                "home_view.logout",
+                error_title="Session Timed Out",
+                error_message="Please sign in again",
+            )
+        )
+    logthis.debug(
+        f"[{session.get('url')}] {session.get('username').title()} viewed {request.path}"
+    )
+    with open(log_file, "r") as fin:
+        lines = [re.sub("\n", "", line) for line in fin.readlines()]
         lines.reverse()
         view_lines = lines[:500]
-    return render_template('log/home.html', username=session.get('username'), log=view_lines)
+    return render_template(
+        "log/home.html", username=session.get("username"), log=view_lines
+    )
 
 
-@blueprint.route('/log/live.html', methods=['GET'])
+@blueprint.route("/log/live.html", methods=["GET"])
 @response(template_file="log/live.html")
-def stream():
-    if 'username' not in session:
-        return redirect(url_for('home_view.logout', error_title="Session Timed Out", error_message="Please sign in again"))
-    logthis.debug(f"[{session.get('url')}] {session.get('username').title()} viewed {request.path}")
+def stream() -> Response:
+    if "username" not in session:
+        return redirect(
+            url_for(
+                "home_view.logout",
+                error_title="Session Timed Out",
+                error_message="Please sign in again",
+            )
+        )
+    logthis.debug(
+        f"[{session.get('url')}] {session.get('username').title()} viewed {request.path}"
+    )
 
     def generate():
         with open(log_file) as f:
             while True:
                 yield f.read()
 
-    return current_app.response_class(generate(), mimetype='text/plain')
+    return current_app.response_class(generate(), mimetype="text/plain")
 
 
-@blueprint.route('/log/yield')
-def yield_log():
-    if 'username' not in session:
-        return redirect(url_for('home_view.logout', error_title="Session Timed Out", error_message="Please sign in again"))
-    logthis.info(f"/log/yield accessed by {session.get('username') or 'nobody'}")
+@blueprint.route("/log/yield")
+def yield_log() -> Response:
+    if "username" not in session:
+        return redirect(
+            url_for(
+                "home_view.logout",
+                error_title="Session Timed Out",
+                error_message="Please sign in again",
+            )
+        )
+    logthis.info(
+        f"/log/yield accessed by {session.get('username') or 'nobody'}"
+    )
 
     def inner():
         proc = subprocess.Popen(
-            [f'tail -f {log_file}'],  # call something with a lot of output so we can see it
+            [
+                f"tail -f {log_file}"
+            ],  # call something with a lot of output so we can see it
             shell=True,
-            stdout=subprocess.PIPE
+            stdout=subprocess.PIPE,
         )
 
-        for line in iter(proc.stdout.readline, ''):
-            sleep(.1)  # Don't need this just shows the text streaming
-            yield line.rstrip().decode() + '<br/>\n'
+        for line in iter(proc.stdout.readline, ""):
+            sleep(0.1)  # Don't need this just shows the text streaming
+            yield line.rstrip().decode() + "<br/>\n"
 
-    return Response(inner(), mimetype='text/html')
+    return Response(inner(), mimetype="text/html")
 
 
-@blueprint.route('/log/download')
-def download_logs():
-    if 'username' not in session:
-        return redirect(url_for('home_view.logout', error_title="Session Timed Out", error_message="Please sign in again"))
-    logthis.info(f"[{session.get('url')}] {session.get('username')} used {request.path} to download the log.")
+@blueprint.route("/log/download")
+def download_logs() -> Response:
+    if "username" not in session:
+        return redirect(
+            url_for(
+                "home_view.logout",
+                error_title="Session Timed Out",
+                error_message="Please sign in again",
+            )
+        )
+    logthis.info(
+        f"[{session.get('url')}] {session.get('username')} used {request.path} to download the log."
+    )
     timestamp = datetime.now()
     logthis.info(f"Downloading log file...{timestamp}-jawa.log")
-    return send_file(log_file, as_attachment=True, download_name=f"{datetime.now()}-jawa.log")
+    return send_file(
+        log_file,
+        as_attachment=True,
+        download_name=f"{datetime.now()}-jawa.log",
+    )
